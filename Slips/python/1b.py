9fba5d7b6d48fc9758580fa66ff06d26b7a57c8f
HighnessAtharva <HighnessAtharva@gmail.com>
2023-01-06 13:17:50 +0530

    1b quickfix

--- Slips/python/1b.py
+++ Slips/python/1b.py
@@ -1,4 +1,8 @@
 
 # Write a python program to accept string and remove the characters which have odd index values of given string using user defined function.
 
-print(input("Enter a string: ")[::2])
+def remove_odd_index(text):
+    print(text[::2])
+
+
+remove_odd_index(input("Enter a string: "))
