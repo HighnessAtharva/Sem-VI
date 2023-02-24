92dd86353b31f9b180e5acf653e01ac93c01b539
Anay <anaydesh1234@gmail.com>
2023-02-24 22:15:28 +0530

    Update 15a.py

--- Slips/python/15a.py
+++ Slips/python/15a.py
@@ -1,7 +1,5 @@
 # Write a Python program to accept two lists and merge the two lists into list of tuple.
 
 print(
-    [tuple(
-        int(input("Enter a number for list #1: ")) for _ in range(5)), tuple(int(input("Enter another number for list #2: ")) for _ in range(5))
-     ]
+    [tuple(int(input("Enter a number for list #1: ")) for _ in range(5)), tuple(int(input("Enter another number for list #2: ")) for _ in range(5))]
 )
