c39287221b9a998cec8c7d3ffd214950e29df8d4
HighnessAtharva <HighnessAtharva@gmail.com>
2023-02-25 12:02:16 +0530

    slips + notes updated

--- Slips/python/13a.py
+++ Slips/python/13a.py
@@ -1,13 +1,34 @@
-# Write a python script to implement bubble sort using list.
+# REPLACED SLIP QUESTION -> PREVIOUSLY: BUBBLE SORT | UPDATED TO: SELECT SORT
 
-def bubble_sort(list):
-    N= len(list)
-    for i in range(N):
-        for j in range(N-1):
-            if list[j] > list[j+1]:
-                list[j], list[j+1] = list[j+1], list[j]
-    return list
+# ------------------------------
+# BUBBLE SORT
+# ------------------------------
 
-print("Sorted List:")
-print(bubble_sort([9, 8, 1, 2, 3, 4, 6, 5, 10, 7]))
+# def bubble_sort(list):
+#     N= len(list)
+#     for i in range(N):
+#         for j in range(N-1):
+#             if list[j] > list[j+1]:
+#                 list[j], list[j+1] = list[j+1], list[j]
+#     return list
 
+# print("Sorted List:")
+# print(bubble_sort([9, 8, 1, 2, 3, 4, 6, 5, 10, 7]))
+
+
+# ------------------------------
+# SELECTION SORT
+# ------------------------------
+def selectionSort(array, size):
+    for step in range(size):
+        min_idx = step
+        for i in range(step + 1, size):
+            if array[i] < array[min_idx]:
+                min_idx = i
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+data = [-2, 45, 0, 11, -9]
+size = len(data)
+selectionSort(data, size)
+print('Sorted Array in Ascending Order:')
+print(data)
--- Slips/python/2a.py
+++ Slips/python/2a.py
@@ -1,14 +1,27 @@
+# REPLACED SLIP QUESTION -> PREVIOUSLY: ARMSTRONG VALIDATION | UPDATED TO: PALINDROME VALIDATION
+
 # Write a Python program to print whether a given number is Armstrong or not.
 
-def armstrong(num):
-    sum = 0
-    temp = num
-    while temp:
-        rem= temp % 10
-        sum +=rem ** 3
-        temp //= 10
-    return f"{num} is an armstrong number" if num == sum else f"{num} is not an armstrong number"
+# def armstrong(num):
+#     sum = 0
+#     temp = num
+#     while temp:
+#         rem= temp % 10
+#         sum +=rem ** 3
+#         temp //= 10
+#     return f"{num} is an armstrong number" if num == sum else f"{num} is not an armstrong number"
+
+# print(armstrong(153))
+# print(armstrong(154))
 
 
-print(armstrong(153))
-print(armstrong(154))
+# ------------------------------
+# PALINDROME VALIDATION
+# ------------------------------
+
+def palindrome(num):
+    num=str(num)
+    print(f"{num} is a palindrome" if num == num[::-1] else f"{num} is not a palindrome")
+
+palindrome(1232)
+palindrome(1221)
